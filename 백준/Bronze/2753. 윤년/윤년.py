b511848d0ff647703a741bde345bcac708f40022
dankim9215 <dankim9215@gmail.com>
2026-01-06 16:28:18 +0900

    Refactor leap year check with comments
    Added comments to clarify leap year conditions.

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,7 @@
 year = int(input())
 
-if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0): # 조건 4의 배수이면서(and) (or) 100이 아니거나 400의 배수여야함
 	print('1')
 else:
+
 	print('0')
