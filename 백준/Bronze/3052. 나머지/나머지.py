80834530bef60cdab0a585420df498c59c5a0f27
dankim9215 <dankim9215@gmail.com>
2026-01-08 09:32:08 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,16 +1,1 @@
-
-li_ = []
-
-for _ in range(10):
-    num = int(input())
-    li_.append(num % 42)
-
-li2_ = []
-for a in range(10):
-    for b in range(10):
-        if li_[a] == li_[b]:
-            li2_.append(li_[a])
-        b += 1
-    a += 1
-    
-print(len((set(li2_))))
+print(len({int(input()) % 42 for _ in range(10)}))
