629948bf214ae7a1217dc1ca4b0ea4cb7e848b89
dankim9215 <dankim9215@gmail.com>
2026-01-11 13:16:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -3,12 +3,6 @@ a, b = map(int, input().split())
 li1 = [list(map(int, input().split())) for _ in range(a)]
 li2 = [list(map(int, input().split())) for _ in range(a)]
 
-result = []
-for i in range(a):
-	row = []
-	for j in range(b):
-		row.append(li1[i][j] + li2[i][j])
-	result.append(row)
-
-for k in result:
-	print(*k)
+for r1, r2 in zip(li1, li2):
+    row = [c1 + c2 for c1, c2 in zip(r1, r2)]
+    print(*row)
